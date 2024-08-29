2fd81c07daa83e1fee19522da351a9211265e1c8
bozanfaruk <bozanfaruk@gmail.com>
2024-08-29 09:53:15 +0300

    design-patterns module added.

---
+++ design-patterns/src/main/java/com/farukbozan/projecte/designpatterns/DesignPatternsMain.java
@@ -0,0 +1,11 @@
+package com.farukbozan.projecte.designpatterns;
+
+import java.util.logging.Logger;
+
+public class DesignPatternsMain {
+
+    public static void main(String[] args) {
+        Logger.getLogger(DesignPatternsMain.class.getName()).info("Design Patterns");
+    }
+
+}
