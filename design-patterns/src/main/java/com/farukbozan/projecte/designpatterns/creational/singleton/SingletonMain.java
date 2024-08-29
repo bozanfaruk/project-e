7e31ba8e8f00032e0391a30f42936b1c3e025be9
bozanfaruk <bozanfaruk@gmail.com>
2024-08-29 15:50:06 +0300

    Singleton design pattern package created.

---
+++ design-patterns/src/main/java/com/farukbozan/projecte/designpatterns/creational/singleton/SingletonMain.java
@@ -0,0 +1,11 @@
+package com.farukbozan.projecte.designpatterns.creational.singleton;
+
+import java.util.logging.Logger;
+
+public class SingletonMain {
+
+    public static void main(String[] args) {
+        Logger.getLogger(SingletonMain.class.getName()).info("Singleton Design Pattern");
+    }
+
+}
